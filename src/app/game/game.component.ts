9a9753c867b5819d74229afbe20e290a50e9747e
katecourt <kate.court@newcastle.ac.uk>
2020-05-01 15:52:08 +0100

    player 1 icon moves

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,12 +12,24 @@ p2Square;
 currentPlayer;
 winningMessage;
 lastRoll;
+
+// counter positioning
+p1Top;
+p1Left;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
     this.currentPlayer = 1;
     this.p1Square = 1;
     this.p2Square = 1;
+    this.moveP1Icon(1);
+  }
+
+  step1On() {
+    this.move(1);
   }
 
   move(numberRolled) {
@@ -37,6 +49,7 @@ lastRoll;
     // move player
     if (this.currentPlayer === 1) {
       this.p1Square = moveTo;
+      this.moveP1Icon(moveTo);
     } else {
       this.p2Square = moveTo;
     }
@@ -69,6 +82,87 @@ lastRoll;
     this.currentPlayer = 1;
     this.p1Square = 1;
     this.p2Square = 1;
+    this.moveP1Icon(1);
+  }
+
+
+  moveP1Icon(moveTo) {
+  switch(moveTo) {
+    case 1:
+      this.p1Top = '5%';
+      this.p1Left = '10%';
+      break;
+    case 2:
+      this.p1Top = '5%';
+      this.p1Left = '25%';
+      break;
+    case 3:
+      this.p1Top = '5%';
+      this.p1Left = '40%';
+      break;
+    case 4:
+      this.p1Top = '5%';
+      this.p1Left = '55%';
+      break;
+    case 5:
+      this.p1Top = '5%';
+      this.p1Left = '70%';
+      break;
+    case 6:
+      this.p1Top = '5%';
+      this.p1Left = '85%';
+      break;
+    case 7:
+      this.p1Top = '24%';
+      this.p1Left = '87%';
+      break;
+    case 8:
+      this.p1Top = '34%';
+      this.p1Left = '87%';
+      break;
+    case 9:
+      this.p1Top = '44%';
+      this.p1Left = '87%';
+      break;
+    case 10:
+      this.p1Top = '87%';
+      this.p1Left = '85%';
+      break;
+    case 11:
+      this.p1Top = '87%';
+      this.p1Left = '70%';
+      break;
+    case 12:
+      this.p1Top = '87%';
+      this.p1Left = '55%';
+      break;
+    case 13:
+      this.p1Top = '87%';
+      this.p1Left = '40%';
+      break;
+    case 14:
+      this.p1Top = '87%';
+      this.p1Left = '40%';
+      break;
+    case 15:
+      this.p1Top = '87%';
+      this.p1Left = '25%';
+      break;
+    case 16:
+      this.p1Top = '44%';
+      this.p1Left = '13%';
+      break;
+    case 17:
+      this.p1Top = '34%';
+      this.p1Left = '13%';
+      break;
+    case 18:
+      this.p1Top = '24%';
+      this.p1Left = '13%';
+      break;
+
+  }
+
   }
 
 }
