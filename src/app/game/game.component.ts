2cfecdf5f2abafd4ef94133413c9c65cb72d4588
katecourt <kate.court@newcastle.ac.uk>
2020-05-04 11:33:23 +0100

    both counters move around board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -16,6 +16,8 @@ lastRoll;
 // counter positioning
 p1Top;
 p1Left;
+p2Top;
+p2Left;
 
 
 
@@ -26,10 +28,16 @@ p1Left;
     this.p1Square = 1;
     this.p2Square = 1;
     this.moveP1Icon(1);
+    this.moveP2Icon(1);
   }
 
   step1On() {
     this.move(1);
+  }
+  step2On() {
+    const current = this.p2Square;
+    this.p2Square = current + 1;
+    this.moveP2Icon(this.p2Square);
   }
 
   move(numberRolled) {
@@ -83,86 +91,165 @@ p1Left;
     this.p1Square = 1;
     this.p2Square = 1;
     this.moveP1Icon(1);
+    this.moveP2Icon(1);
   }
 
 
+  // move icons around 2 'tracks' around the board using their relative position to the board
   moveP1Icon(moveTo) {
   switch(moveTo) {
     case 1:
-      this.p1Top = '5%';
-      this.p1Left = '10%';
+      this.p1Top = '10%';
+      this.p1Left = '5%';
       break;
     case 2:
-      this.p1Top = '5%';
-      this.p1Left = '25%';
+      this.p1Top = '10%';
+      this.p1Left = '20%';
       break;
     case 3:
-      this.p1Top = '5%';
-      this.p1Left = '40%';
+      this.p1Top = '10%';
+      this.p1Left = '35%';
       break;
     case 4:
-      this.p1Top = '5%';
-      this.p1Left = '55%';
+      this.p1Top = '10%';
+      this.p1Left = '50%';
       break;
     case 5:
-      this.p1Top = '5%';
-      this.p1Left = '70%';
+      this.p1Top = '10%';
+      this.p1Left = '65%';
       break;
     case 6:
-      this.p1Top = '5%';
-      this.p1Left = '85%';
+      this.p1Top = '10%';
+      this.p1Left = '80%';
       break;
     case 7:
       this.p1Top = '24%';
       this.p1Left = '87%';
       break;
     case 8:
-      this.p1Top = '34%';
+      this.p1Top = '41%';
       this.p1Left = '87%';
       break;
     case 9:
-      this.p1Top = '44%';
+      this.p1Top = '60%';
       this.p1Left = '87%';
       break;
     case 10:
-      this.p1Top = '87%';
-      this.p1Left = '85%';
+      this.p1Top = '82%';
+      this.p1Left = '90%';
       break;
     case 11:
-      this.p1Top = '87%';
-      this.p1Left = '70%';
+      this.p1Top = '82%';
+      this.p1Left = '75%';
       break;
     case 12:
-      this.p1Top = '87%';
-      this.p1Left = '55%';
+      this.p1Top = '82%';
+      this.p1Left = '60%';
       break;
     case 13:
-      this.p1Top = '87%';
-      this.p1Left = '40%';
+      this.p1Top = '82%';
+      this.p1Left = '45%';
       break;
     case 14:
-      this.p1Top = '87%';
-      this.p1Left = '40%';
+      this.p1Top = '82%';
+      this.p1Left = '30%';
       break;
     case 15:
-      this.p1Top = '87%';
-      this.p1Left = '25%';
+      this.p1Top = '82%';
+      this.p1Left = '15%';
       break;
     case 16:
-      this.p1Top = '44%';
-      this.p1Left = '13%';
+      this.p1Top = '70%';
+      this.p1Left = '8%';
       break;
     case 17:
-      this.p1Top = '34%';
-      this.p1Left = '13%';
+      this.p1Top = '50%';
+      this.p1Left = '8%';
       break;
     case 18:
-      this.p1Top = '24%';
-      this.p1Left = '13%';
+      this.p1Top = '32%';
+      this.p1Left = '8%';
       break;
 
   }
 
+  }
+
+  moveP2Icon(moveTo) {
+    switch (moveTo) {
+      case 1:
+        this.p2Top = '10%';
+        this.p2Left = '14%';
+        break;
+      case 2:
+        this.p2Top = '10%';
+        this.p2Left = '29%';
+        break;
+      case 3:
+        this.p2Top = '10%';
+        this.p2Left = '44%';
+        break;
+      case 4:
+        this.p2Top = '10%';
+        this.p2Left = '59%';
+        break;
+      case 5:
+        this.p2Top = '10%';
+        this.p2Left = '75%';
+        break;
+      case 6:
+        this.p2Top = '10%';
+        this.p2Left = '90%';
+        break;
+      case 7:
+        this.p2Top = '32%';
+        this.p2Left = '87%';
+        break;
+      case 8:
+        this.p2Top = '50%';
+        this.p2Left = '87%';
+        break;
+      case 9:
+        this.p2Top = '70%';
+        this.p2Left = '87%';
+        break;
+      case 10:
+        this.p2Top = '82%';
+        this.p2Left = '80%';
+        break;
+      case 11:
+        this.p2Top = '82%';
+        this.p2Left = '65%';
+        break;
+      case 12:
+        this.p2Top = '82%';
+        this.p2Left = '50%';
+        break;
+      case 13:
+        this.p2Top = '82%';
+        this.p2Left = '35%';
+        break;
+      case 14:
+        this.p2Top = '82%';
+        this.p2Left = '20%';
+        break;
+      case 15:
+        this.p2Top = '82%';
+        this.p2Left = '5%';
+        break;
+      case 16:
+        this.p2Top = '60%';
+        this.p2Left = '8%';
+        break;
+      case 17:
+        this.p2Top = '41%';
+        this.p2Left = '8%';
+        break;
+      case 18:
+        this.p2Top = '24%';
+        this.p2Left = '8%';
+        break;
+    }
   }
 
 }
