75cc6280fd331b9f57db70e3933bff391c3426c9
katecourt <kate.court@newcastle.ac.uk>
2020-05-13 15:55:50 +0100

    start counter highlights

--- src/app/counters/counters.component.ts
+++ src/app/counters/counters.component.ts
@@ -24,7 +24,9 @@ player2EnteredName: boolean;
 showCounterAlert = false;
 
 player1Counter;
+player1CounterNumber;
 player2Counter;
+player2CounterNumber;
 
 
 // label counters
@@ -61,27 +63,35 @@ player2Name;
       switch (counterChoice) {
         case 1:
           this.player1Counter = this.one;
+          this.player1CounterNumber = 1;
           break;
         case 2:
           this.player1Counter = this.two;
+          this.player1CounterNumber = 2;
           break;
         case 3:
           this.player1Counter = this.three;
+          this.player1CounterNumber = 3;
           break;
         case 4:
           this.player1Counter = this.four;
+          this.player1CounterNumber = 4;
           break;
         case 5:
           this.player1Counter = this.five;
+          this.player1CounterNumber = 5;
           break;
         case 6:
           this.player1Counter = this.six;
+          this.player1CounterNumber = 6;
           break;
         case 7:
           this.player1Counter = this.seven;
+          this.player1CounterNumber = 7;
           break;
         case 8:
           this.player1Counter = this.eight;
+          this.player1CounterNumber = 8;
           break;
       }
       console.log('play' + this.player1Counter + '.mp3');
@@ -93,28 +103,39 @@ player2Name;
     } else if (this.player2Choosing ===  true && this.player2EnteredName === true) {
       switch (counterChoice) {
         case 1:
-          this.player2Counter = this.one;
+          if (this.player1CounterNumber === 1) {
+          return;
+          }
+            this.player2Counter = this.one;
+            this.player2CounterNumber = 1;
           break;
         case 2:
           this.player2Counter = this.two;
+          this.player2CounterNumber = 2;
           break;
         case 3:
           this.player2Counter = this.three;
+          this.player2CounterNumber = 3;
           break;
         case 4:
           this.player2Counter = this.four;
+          this.player2CounterNumber = 4;
           break;
         case 5:
           this.player2Counter = this.five;
+          this.player2CounterNumber = 5;
           break;
         case 6:
           this.player2Counter = this.six;
+          this.player2CounterNumber = 6;
           break;
         case 7:
           this.player2Counter = this.seven;
+          this.player2CounterNumber = 7;
           break;
         case 8:
           this.player2Counter = this.eight;
+          this.player2CounterNumber = 8;
           break;
       }
       console.log('play' + this.player1Counter + '.mp3');
@@ -124,6 +145,7 @@ player2Name;
       audio.play();
     }
   }
+
 
  player1NameEntered() {
 this.player1EnteredName = true;
@@ -172,5 +194,9 @@ if (this.player2Counter !== undefined) {
 
  }
 
+ checkIfCardIsClicked(num) {
+    if (this.player1CounterNumber === num) {}
+
+ }
 
 }
