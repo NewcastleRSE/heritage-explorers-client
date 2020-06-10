c9151d2b57282140d2a7588e5c2204c4c75baaed
katecourt <kate.court@newcastle.ac.uk>
2020-06-10 14:49:27 +0100

    adjust game board player 2 pool position

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -94,13 +94,13 @@ missAGoAlertClosed = true;
     if (this.globalsService.player1Name !== undefined) {
       this.p1Name = this.globalsService.player1Name;
     } else {
-      this.p1Name = 'Player 1';
+      this.p1Name = 'Blue';
     }
 
     if (this.globalsService.player2Name !== undefined) {
       this.p2Name = this.globalsService.player2Name;
     } else {
-      this.p2Name = 'Player 2';
+      this.p2Name = 'Yellow';
     }
   }
 
